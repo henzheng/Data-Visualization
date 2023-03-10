611a785f0150734e6ebd212a9b85df0083433119
Henry Zheng <zhenghenry2@gmail.com>
2023-03-10 10:30:26 -0500

    adding pokemon data

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svg id="canvas" width="600" height="400"></svg>
+
+<script>
+    import { onMount } from 'svelte';
+    import * as d3 from 'd3';
+    onMount(() => {
+        const data = [10, 100, 50, 35]
+        const canvas = d3.select('#canvas');
+        const circles = canvas.selectAll('circle')
+        .data(data);
+        circles.enter().append('circle')
+        .attr('cx', 100)
+        .attr('cy', 50)
+        .attr('r', 50)
+        .attr('fill', 'red')
+    })
+</script>
